ed3a537aaf72f217c45e61c8c56a47991303ceeb
SnowHana <wujindaniel1011@gmail.com>
2024-09-13 17:31:33 +0900

    Basic implementation of player stats graph is done.

--- src/footy/base/urls.py
+++ src/footy/base/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("players-all/", views.players_all, name="players_all"),
     path("player-stats/<slug:slug>", views.player_stats, name="player_stats"),
+    path(
+        "player-stats/<slug:slug>/graph/", views.generate_graph, name="generate_graph"
+    ),
 ]
