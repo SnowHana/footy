253195850c0337420134098c1dfcdf576399d807
SnowHana <wujindaniel1011@gmail.com>
2024-09-28 07:44:36 +0900

    Planning to change model name from Team to Club cuz thats what our dataset uses

--- src/footy/base/models.py
+++ src/footy/base/models.py
@@ -7,6 +7,7 @@ class Team(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=255, unique=True)
 
+    elo = 
     def __str__(self):
         return self.name
 
