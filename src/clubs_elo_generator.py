f8b41a5b05185105e9382a2793d95878f646f3e8
SnowHana <wujindaniel1011@gmail.com>
2024-10-07 12:21:31 +0900

    clubs_elo still sucks. Probs atm, start to working on displaying my clubs_elo and then later we can improve it?

--- src/clubs_elo_generator.py
+++ src/clubs_elo_generator.py
@@ -92,8 +92,16 @@ def expected_chance_of_success(home_team_elo, away_team_elo) -> int:
     """
     return 1 / (1 + 10 ** ((away_team_elo - home_team_elo) / 400))
 
+    # def decide_K_value(game: pd.Series):
+    """Decide K value based on league or etc?
 
-def calculate_new_elo(home_team_elo, away_team_elo, result, K=30):
+    Args:
+        game (pd.Series): _description_
+    """
+    # if game["competition"] == ''
+
+
+def calculate_new_elo(clubs_df: pd.DataFrame, game: pd.Series, K=20):
     """Calculate new elo
 
     Args:
@@ -105,16 +113,65 @@ def calculate_new_elo(home_team_elo, away_team_elo, result, K=30):
     Returns:
         tuple (int, int): new elos of home and away team
     """
+    result = game["is_win"]
+
     if result not in [-1, 0, 1]:
         raise AssertionError("Invalid result value passed!")
 
-    expected_home_team = expected_chance_of_success(home_team_elo, away_team_elo)
+    home_club_id = game["home_club_id"]
+    away_club_id = game["away_club_id"]
+
+    home_club_elo = clubs_df.loc[clubs_df["club_id"] == home_club_id, "elo"].values[0]
+    away_club_elo = clubs_df.loc[clubs_df["club_id"] == away_club_id, "elo"].values[0]
+
+    expected_home_team = expected_chance_of_success(home_club_elo, away_club_elo)
     expected_away_team = 1 - expected_home_team
 
-    new_home_elo = home_team_elo + K * (result - expected_home_team)
-    new_away_elo = away_team_elo + K * ((1 - result) - expected_away_team)
+    new_home_elo = home_club_elo + K * (result - expected_home_team)
+    new_away_elo = away_club_elo + K * ((1 - result) - expected_away_team)
 
     return new_home_elo, new_away_elo
+
+
+def clean_games_df(games_df: pd.DataFrame, clubs_df: pd.DataFrame) -> pd.DataFrame:
+    """Clean games_df so that it only contains games between clubs that
+    exists in clubs_df
+
+    Args:
+        games_df (pd.DataFrame): _description_
+        clubs_df (pd.DataFrame): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+
+    # Clean games_df so that it only contains games that have valid club id (ie. Clubs in our club_df)
+    home_exists = games_df["home_club_id"].isin(clubs_df["club_id"])
+    away_exists = games_df["away_club_id"].isin(clubs_df["club_id"])
+    cleaned_games_df = games_df[home_exists & away_exists]
+
+    return cleaned_games_df
+
+
+def calculate_clubs_elo_single_match(clubs_df: pd.DataFrame, row):
+
+    home_club_id = row["home_club_id"]
+    away_club_id = row["away_club_id"]
+
+    # Get current ELOs for home and away clubs from clubs_df
+    home_elo = clubs_df.loc[clubs_df["club_id"] == home_club_id, "elo"].values[0]
+    away_elo = clubs_df.loc[clubs_df["club_id"] == away_club_id, "elo"].values[0]
+
+    # Get result for the game (assuming 1 for home win, 0 for away win, 0.5 for draw)
+    game_result = row["is_win"]  # Adjust this based on your game logic
+
+    # Update ELOs based on the result
+    new_home_elo, new_away_elo = calculate_new_elo(clubs_df, row)
+
+    clubs_df.loc[clubs_df["club_id"] == home_club_id, "elo"] = new_home_elo
+    clubs_df.loc[clubs_df["club_id"] == away_club_id, "elo"] = new_away_elo
+
+    return clubs_df
 
 
 def calculate_clubs_elo():
@@ -131,20 +188,18 @@ def calculate_clubs_elo():
 
     # Import data
     dataframes = import_data_from_csv()
-    # print(dataframes)
     games_df = dataframes["games_df"]
     clubs_df = dataframes["clubs_df"]
     club_games_df = dataframes["club_games_df"]
 
-    # Clean games_df so that it only contains games that have valid club id (ie. Clubs in our club_df)
-    home_exists = games_df["home_club_id"].isin(clubs_df["club_id"])
-    away_exists = games_df["away_club_id"].isin(clubs_df["club_id"])
-    cleaned_games_df = games_df[home_exists & away_exists]
+    cleaned_games_df = clean_games_df(games_df, clubs_df)
 
     # Merge
     club_games_merged_df = pd.merge(
         cleaned_games_df, club_games_df, on="game_id", how="inner"
     )
+
+    club_games_merged_df.to_csv("club_games_merged.csv")
 
     # Now, we will sort by date (Oldest to Latest)
     # To calculate and update ELO by time
@@ -152,30 +207,31 @@ def calculate_clubs_elo():
     club_games_merged_df = sort_games_by_date(club_games_merged_df)
 
     # Initialise club elos
-    # NOTE: Here we are using 1000, but better init. can be done like considering team market value
+    # NOTE: Here we are using 1500, but better init. can be done like considering team market value
     # or level of league they are playing for etc
-    clubs_df["elo"] = 1000
+    clubs_df["elo"] = 1500
     # Ensure the 'elo' column is of type float
     clubs_df["elo"] = clubs_df["elo"].astype(float)
     test_games_df = club_games_merged_df.copy()
 
     update_counter = 0
     for idx, row in test_games_df.iterrows():
-        home_club_id = row["home_club_id"]
-        away_club_id = row["away_club_id"]
+        # home_club_id = row["home_club_id"]
+        # away_club_id = row["away_club_id"]
 
-        # Get current ELOs for home and away clubs from clubs_df
-        home_elo = clubs_df.loc[clubs_df["club_id"] == home_club_id, "elo"].values[0]
-        away_elo = clubs_df.loc[clubs_df["club_id"] == away_club_id, "elo"].values[0]
+        # # Get current ELOs for home and away clubs from clubs_df
+        # home_elo = clubs_df.loc[clubs_df["club_id"] == home_club_id, "elo"].values[0]
+        # away_elo = clubs_df.loc[clubs_df["club_id"] == away_club_id, "elo"].values[0]
 
-        # Get result for the game (assuming 1 for home win, 0 for away win, 0.5 for draw)
-        game_result = row["is_win"]  # Adjust this based on your game logic
+        # # Get result for the game (assuming 1 for home win, 0 for away win, 0.5 for draw)
+        # game_result = row["is_win"]  # Adjust this based on your game logic
 
-        # Update ELOs based on the result
-        new_home_elo, new_away_elo = calculate_new_elo(home_elo, away_elo, game_result)
+        # # Update ELOs based on the result
+        # new_home_elo, new_away_elo = calculate_new_elo(home_elo, away_elo, game_result)
 
-        clubs_df.loc[clubs_df["club_id"] == home_club_id, "elo"] = new_home_elo
-        clubs_df.loc[clubs_df["club_id"] == away_club_id, "elo"] = new_away_elo
+        # clubs_df.loc[clubs_df["club_id"] == home_club_id, "elo"] = new_home_elo
+        # clubs_df.loc[clubs_df["club_id"] == away_club_id, "elo"] = new_away_elo
+        clubs_df = calculate_clubs_elo_single_match(clubs_df, row)
 
         update_counter += 1
 
